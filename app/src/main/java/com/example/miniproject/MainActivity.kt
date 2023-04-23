9be8d6240d86c4b33b056a3f1566bc0c85ed9099
luna <eskw0701@naver.com>
2023-04-23 18:40:27 +0900

    [StudyProject] FineDustResponse.kt.. Call<> 에러.. 왜용..ㅠㅠ

--- app/src/main/java/com/example/miniproject/MainActivity.kt
+++ app/src/main/java/com/example/miniproject/MainActivity.kt
@@ -1,40 +1,63 @@
 package com.example.miniproject
 
+import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.miniproject.data.BASE_URL
+import com.example.miniproject.data.FineDustBody
+import com.example.miniproject.data.FineDustService
 import com.example.miniproject.ui.theme.MiniProjectTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MiniProjectTheme(true) {
-                // A surface container using the 'background' color from the theme
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+
+        fun getJSONData(courseList: MutableList<String>, ctx: Context){
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            
+            val fineDustService = retrofit.create(FineDustService::class.java)
+            
+            val call: List<FineDustBody> = fineDustService.getFineDust(2023)
+            
+            fineDustService
+                .getFineDust(2022)
+                .enqueque(object: Callback<FineDustBody>{
+                    override fun onResponse(call: Call<FineDustBody>, response: Response<FineDustBody>) {
+                        if(response.isSuccessful){
+
+                        }
+                    }
+
+                    override fun onFailure(call: Call<FineDustBody>, t: Throwable) {
+                        Log.d("로그", t.message.toString())
+                    }
+                })
+
+            setContent {
+                MiniProjectTheme(true) {
+
                 }
             }
+            
         }
-    }
-}
+        
+        
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+        CoroutineScope(Dispatchers.Main).launch{
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MiniProjectTheme {
-        Greeting("Android")
+        }
     }
 }
--- app/src/main/java/com/example/miniproject/data/FineDustResponse.kt
+++ app/src/main/java/com/example/miniproject/data/FineDustResponse.kt
@@ -1,11 +1,31 @@
 package com.example.miniproject.data
 
+import com.google.gson.annotations.SerializedName
+
+data class FineDustList(
+    @SerializedName("response") val list: FineDustResponse
+)
+
 data class FineDustResponse(
-    val totalCount: Int,
-    val items: List<FineDustBody>
+    @SerializedName("items") val items: List<FineDustBody>
 )
 
 data class FineDustBody(
+    @SerializedName("clearVal") val clearVal: String,
+    @SerializedName("sn") val sn: String,
+    @SerializedName("districtName") val districtName: String,
+    @SerializedName("dataDate") val dataDate: String,
+    @SerializedName("issueVal") val issueVal: String,
+    @SerializedName("issueTime") val issueTime: String,
+    @SerializedName("clearDate") val clearDate: String,
+    @SerializedName("issueDate") val issueDate: String,
+    @SerializedName("moveName") val moveName: String,
+    @SerializedName("clearTime") val clearTime: String,
+    @SerializedName("issueGbn") val issueGbn: String,
+    @SerializedName("itemCode") val itemCode: String,
+)
+
+data class FineDustBody1(
     val clearVal: String,
     val sn: String,
     val districtName: String,
@@ -13,8 +33,8 @@ data class FineDustBody(
     val issueVal: String,
     val issueTime: String,
     val clearDate: String,
-    val issueDate: String,
     val moveName: String,
+    val issueDate: String,
     val clearTime: String,
     val issueGbn: String,
     val itemCode: String,
