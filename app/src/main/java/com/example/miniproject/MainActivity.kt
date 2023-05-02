8dc8f3449d5511c31b485143be250e1a20c029d2
luna <eskw0701@naver.com>
2023-05-02 23:27:44 +0900

    movie overview 가져오기

--- app/src/main/java/com/example/miniproject/MainActivity.kt
+++ app/src/main/java/com/example/miniproject/MainActivity.kt
@@ -7,10 +7,7 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
@@ -21,36 +18,39 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.*
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.miniproject.common.Constant.API_KEY_MOVIE
-import com.example.miniproject.common.Constant.BASE_URL_MOVIE
 import com.example.miniproject.data.*
 import com.example.miniproject.service.TheMovieService
 import com.example.miniproject.ui.theme.MiniProjectTheme
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
-    // private val retrofit by lazy { retrofitBuilder() }
-    //private val dustService by lazy { retrofit.create(FineDustService::class.java) }
-
     @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            val courseList = remember { mutableStateListOf<String>() }
-            movieServiceLoad(courseList, applicationContext)
+            val nameList = remember { mutableStateListOf<String>() }
+            movieServiceLoad(nameList, applicationContext)
 
             MiniProjectTheme(true) {
                 Scaffold() {
-                    LazyColumn {
-                        items(courseList) { movie ->
-                            MovieCard(orden = movie)
+                    Text(
+                        text = "현재 영화 TOP20",
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier.padding(15.dp, 15.dp, 15.dp, 20.dp)
+                    )
+                    LazyColumn(modifier = Modifier.padding(0.dp, 45.dp)) {
+                        items(nameList) { movie ->
+                            MovieCard(name = movie)
                         }
                     }
                 }
@@ -59,61 +59,42 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun MovieCard(orden: String) {
+    fun MovieCard(name: String) {
         Card(
             Modifier
                 .padding(12.dp)
                 .border(width = 1.dp, color = Color.White)
+                .height(300.dp)
                 .fillMaxWidth()
-                .height(50.dp)
         ) {
-            Box(contentAlignment = Alignment.Center) {
-                Text("$orden")
+            Box(
+                contentAlignment = Alignment.CenterStart,
+            ) {
+                Text("$name")
             }
         }
     }
-    private fun movieServiceLoad(courseList: MutableList<String>, ctx: Context) {
-        val movieRetrofit = RetrofitClient.movieRetrofit
-        movieRetrofit.create(TheMovieService::class.java).getQuery(
-            courseList.toString(),
-            API_KEY_MOVIE,
-            "popularity.desc",
-            "ko"
-        ).enqueue(object : Callback<MovieListResponse> {
-            override fun onResponse(call: Call<MovieListResponse>, response: Response<MovieListResponse>) {
-                if (response.isSuccessful) {
-                    response.body()?.results?.map {
-                        it.title
-                        it.release_date
-                        courseList.add(it.title)
-                    }
+}
+
+private fun movieServiceLoad(nameList: MutableList<String>, ctx: Context) {
+    val movieRetrofit = RetrofitClient.movieRetrofit
+    movieRetrofit.create(TheMovieService::class.java).getQuery(
+        nameList.toString(),
+        API_KEY_MOVIE,
+        "popularity.desc",
+        "ko"
+    ).enqueue(object : Callback<MovieListResponse> {
+        override fun onResponse(call: Call<MovieListResponse>, response: Response<MovieListResponse>) {
+            if (response.isSuccessful) {
+                response.body()?.results?.map {
+                    nameList.add("${it.title}" + "\n\n${it.overview}")
                 }
             }
-            override fun onFailure(call: Call<MovieListResponse>, t: Throwable) {
-                Log.d("실패", t.message.toString())
-            }
-        })
-    }
+        }
 
-//
-//    var gson = GsonBuilder().setLenient().create()
-//    private fun retrofitBuilder(): Retrofit = Retrofit.Builder()
-//        .baseUrl(BASE_URL)
-//        .addConverterFactory(GsonConverterFactory.create(gson))
-//        .build()
-//
-//    private fun getDustData(): Unit {
-//        val dustRoot: Call<DustResponse> = dustService.getFineDust(2022, "$RETURN_TYPE", "$API_KEY")
-//        dustRoot.enqueue(object : Callback<DustResponse> {
-//
-//            override fun onResponse(call: Call<DustResponse>, response: Response<DustResponse>) {
-//
-//                Log.d("로그", response.body().toString())
-//            }
-//
-//            override fun onFailure(call: Call<DustResponse>, t: Throwable) {
-//                Log.d("실패", t.message.toString())
-//            }
-//        })
-//    }
+        override fun onFailure(call: Call<MovieListResponse>, t: Throwable) {
+            Log.d("실패", t.message.toString())
+        }
+    })
 }
+
